4f8b75244fa781908058ca2099e5ac577bb31fb4
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-01-22 23:45:53 +0600

    Initial Commit

---
+++ app/src/main/java/com/sifat/thenotesapp/model/Note.kt
@@ -0,0 +1,12 @@
+package com.sifat.thenotesapp.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity (tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id:Int, val noteTitle:String, val notDesc:String
+):Parcelable
