38b25c44fd5a18b0a452ad5bb4230a0f51e78355
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-01-25 00:13:38 +0600

    Initial Commit

--- app/src/main/java/com/sifat/thenotesapp/MainActivity.kt
+++ app/src/main/java/com/sifat/thenotesapp/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.sifat.thenotesapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.sifat.thenotesapp.database.NoteDatabase
+import com.sifat.thenotesapp.databinding.ActivityMainBinding
+import com.sifat.thenotesapp.repesitory.NoteRepository
+import com.sifat.thenotesapp.viewModel.NoteViewModel
+import com.sifat.thenotesapp.viewModel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setUpViewModel()
+    }
+    private fun setUpViewModel(){
+        val NoteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, NoteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/sifat/thenotesapp/viewModel/NoteViewModel.kt
@@ -0,0 +1,32 @@
+package com.sifat.thenotesapp.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.room.Query
+import com.sifat.thenotesapp.model.Note
+import com.sifat.thenotesapp.repesitory.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app:Application, private val noteRepository: NoteRepository):AndroidViewModel(app) {
+
+    fun addNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+
+    fun updateNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updateNote(note)
+        }
+
+    fun getAllNotes() = noteRepository.getAllNotes()
+    fun searchNotes(query: String?) =
+        noteRepository.searchNote(query)
+
+}
