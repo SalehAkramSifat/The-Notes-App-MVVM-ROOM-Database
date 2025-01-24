7b4834340f10202a0a0820b75013aecf42bdaa92
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-01-25 00:11:02 +0600

    Initial Commit

---
+++ app/src/main/java/com/sifat/thenotesapp/adapter/NoteAdapter.kt
@@ -0,0 +1,51 @@
+package com.sifat.thenotesapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sifat.thenotesapp.databinding.NoteLayoutBinding
+import com.sifat.thenotesapp.fragment.HomeFragmentDirections
+import com.sifat.thenotesapp.model.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(val itembinding: NoteLayoutBinding) : RecyclerView.ViewHolder(itembinding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            NoteLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itembinding.noteTitle.text = currentNote.noteTitle
+        holder.itembinding.noteDesc.text = currentNote.notDesc
+
+        holder.itemView.setOnClickListener {
+            val direction = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(currentNote)
+            it.findNavController().navigate(direction)
+        }
+    }
+}
--- app/src/main/java/com/sifat/thenotesapp/repesitory/NoteRepository.kt
+++ app/src/main/java/com/sifat/thenotesapp/repesitory/NoteRepository.kt
@@ -1,6 +1,16 @@
 package com.sifat.thenotesapp.repesitory
 
+import androidx.room.Query
 import com.sifat.thenotesapp.database.NoteDatabase
+import com.sifat.thenotesapp.model.Note
 
 class NoteRepository(private val db:NoteDatabase) {
+
+    suspend fun insertNote(note:Note) = db.getNoteDao().insertNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+
 }
